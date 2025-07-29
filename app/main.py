6ac5b650f162e758292b484eb76b8e28e6bf2126
Arpan Agrawal <86358036+arpan9422@users.noreply.github.com>
2025-07-29 22:13:42 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ from app.routes.hackrx import router as hackrx_router
 
 from app.logger.logger import LoggingMiddleware  # 👈 import middleware
 
-import os
-import uvicorn
-
 app = FastAPI()
 
 # Add logging middleware
@@ -22,8 +19,3 @@ app.include_router(hackrx_router, prefix="/api/v1/hackrx")
 @app.get("/")
 def root():
     return {"message": "RAG API running"}
-
-# 👇 Add this block at the bottom
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # 8000 for local, $PORT for Render
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
